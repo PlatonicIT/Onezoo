86763add9edf3de91d0ebe9a54de476a0ff8f1ce
Platonic IT <imax.mahbub@gmail.com>
2018-10-31 07:12:27 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,90 @@
 (function ($) {
     "use strict";
     jQuery(document).ready(function() {
+
+        // Custom Navigation Menu
+        var $window = $(window),
+        $window_width = $window.width(),
+        $win_location = window.location.href,
+        $location_split = $win_location.split("#"),
+        $body = $("body"),
+        $mainnav = $("#main-nav"),
+        $nav_link = $("#main-nav li a");
+
+        function nav_handlr(e, a) {
+        var s = e;
+        !0 === (!0 === a || "open" === a || 1 === a) ? s.slideDown(600) : (s.slideUp(500), s.find("li.nav-opened").removeClass("nav-opened").children("ul").slideUp(300))
+        }
+
+        function mobileNav_handlr() {
+            $window.width() < 767 ? ($mainnav.removeClass("nav-mobile"), $mainnav.find(".has-children").removeClass("nav-opened").removeClass("rollover").children("ul").removeAttr("style")) : $mainnav.addClass("nav-mobile")
+        }
+        "ontouchstart" in document.documentElement || $body.addClass("no-touch");
+        var $win_resize = $window_width;
+        $window.on("resize", function() {
+            $win_resize = $window_width
+        });
+        $nav_link.length > 0 && $nav_link.each(function() {
+            $win_location === this.href && "" !== $location_split[1] && $(this).closest("li").addClass("is-active").parent().closest("li").addClass("is-active")
+        });
+        var $navbar = $(".navigation-wrap"),
+        $nav_li = $mainnav.find("li"),
+        $nav_a = $mainnav.find("a");
+        mobileNav_handlr(), $window.on("resize", function() {
+            mobileNav_handlr()
+        }), $nav_li.has("ul").addClass("has-children"), $nav_li.on({
+        mouseenter: function() {
+            $(this).addClass("rollover")
+        },
+        mouseleave: function() {
+            $(this).removeClass("rollover")
+        }
+        }), $nav_a.on("click touchstart", function(a) {
+        var $this = $(this),
+            $this_parent = $this.parent(),
+            $this_link = $this.attr("href");
+        return "click" === a.type && $win_resize > 0 || ($this_parent.hasClass("has-children") ? $this_parent.hasClass("nav-opened") ? ($this_parent.removeClass("nav-opened"), "#" !== $this_link || (nav_handlr($this_parent.children("ul"), "close"), !1)) : ($this_parent.addClass("nav-opened"), $this_parent.siblings().removeClass("nav-opened"), nav_handlr($this_parent.siblings().children("ul"), "close"), setTimeout(function() {
+            nav_handlr($this_parent.children("ul"), "open")
+        }, 150), !1) : "#" !== $this_link && void 0)
+        });
+
+
+        $('.main-nav li.has-children').on( 'click mouseover', function() {
+            $(this).addClass('nav-opened');
+        } );
+
+        $('.main-nav li.has-children').on( 'mouseleave', function() {
+            $(this).removeClass('nav-opened');
+        } );
+
+
+        //add active class on nav
+        $('.main-menu ul').on('click', 'li',function(){
+            // $(this).addClass('is-active').siblings().removeClass('is-active');
+        });
+
+        $('#contact-btn, .contact-btn').on( 'click', function() {
+            $('.contact-popup').toggleClass('active');
+        });
+        
+        $('.hero-wrap').on( 'click', function() {
+            $('.contact-popup').removeClass('active');
+        });
         
         //Title for Icons
         $('[data-toggle="tooltip"]').tooltip();
+
+
+        // Backgrond Slider
+        $('.slider-background').owlCarousel({
+            items: 1,
+            fade: true,
+            autoplay: true,
+            animateOut: 'fadeOut',
+            nav: true,
+            loop: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+        });
         
 
         // Search
@@ -200,6 +281,26 @@
 
         // Activate WOW JS
         new WOW().init();
+
+
+        // Custom Tooltip
+        $(function () {
+            $('.trigger').myTooltip();
+        });          
+        $.fn.myTooltip = function () {            
+            var $this = $(this),
+                $tooltip = $this.find('.my-tooltip');
+            
+            $this.mouseenter(function () {
+                $(this).find('.my-tooltip').addClass('show');
+            }).mouseleave(function () {
+                $(this).find('.my-tooltip').removeClass('show');
+            }); 
+            
+            $tooltip.click(function () {
+                $(this).find('.my-tooltip').removeClass('show');
+            });
+        };
             
     });
 })(jQuery);
